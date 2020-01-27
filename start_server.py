e0e404efa0dda03ced7b04efa114dfab0ba815d5
Eric Renteria <eric.renteria.88@gmail.com>
2020-01-26 17:47:07 -0800

    Move app logic into its own module

--- start_server.py
+++ start_server.py
@@ -1,39 +1,6 @@
-import logging
-import os
-import runpy
-import tempfile
 import uvicorn
 
-from fastapi import FastAPI
-from git import Repo
-
-logger = logging.getLogger(__name__)
-
-app = FastAPI()
-
-
-@app.post('/pipelines/clone-and-run')
-def clone_and_run(git_repo_url: str, pipeline_script_name: str):
-    try:
-        with tempfile.TemporaryDirectory() as tmp_dir_path:
-            logger.info(f'Cloning repository {git_repo_url} to {tmp_dir_path}')
-            Repo.clone_from(url=git_repo_url, to_path=tmp_dir_path)
-
-            pipeline_script_path = os.path.join(tmp_dir_path, pipeline_script_name)
-            logger.info(f'Using runpy to load Pipeline from pipeline script {pipeline_script_path}')
-            context = runpy.run_path(pipeline_script_path)
-
-            pipeline = context['pipeline']
-
-            for step in pipeline.steps:
-                step()
-    except PermissionError as e:
-        error_str = str(e)
-        if 'WinError' in error_str and '.git' in error_str:
-            logger.warning('Unable to delete temporary git directory in Windows')
-
-    return {}
-
+from cinco.app import app
 
 if __name__ == '__main__':
     # TODO: this is just here for testing, organize it better once the project is more complete
